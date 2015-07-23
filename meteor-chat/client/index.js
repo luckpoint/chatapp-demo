73829377a8e839260dfd6de027fd0d0a9939d021
luckpoint <stsutsui@MAC-C-219.local>
2015-07-23 17:25:46 +0900

    meteor-chatのサンプルを追加

---
+++ meteor-chat/client/index.js
@@ -0,0 +1,43 @@
+Template.inputBox.events({
+  'keypress input': function(e) {
+    return newMessage(e);
+  },
+  'click button': function(e) {
+    return newMessage(e, true);
+  }
+});
+
+function newMessage(e, isButtonClick) {
+    var inputVal = $('#input-box-text').val();
+    if(!!inputVal) {
+        var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+        if (charCode == 13 || isButtonClick) {
+            e.stopPropagation();
+            Meteor.call('newMessage', {user: $('#caller-id').val(), text: $('#input-box-text').val()});
+            $('#input-box-text').val("");
+            return false;
+        }    
+    }
+}
+
+Template.textChat.helpers({
+  messages: Messages.find({}, {sort:{timestamp: 1}})
+});
+
+
+Template.registerHelper("timestampToTime", function (timestamp) {
+	var date = new Date(timestamp);
+	var hours = date.getHours();
+	var minutes = "0" + date.getMinutes();
+	var seconds = "0" + date.getSeconds();
+	return hours + ':' + minutes.substr(minutes.length-2) + ':' + seconds.substr(seconds.length-2);
+});
+
+Template.registerHelper("username", function (user) {
+	if (user == null || user == "" || typeof user === "undefined") {
+		return "Anonymous";
+	}
+	return user;
+});
+
+Meteor.subscribe('messages');
